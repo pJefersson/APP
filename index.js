a715458fb5c6f70db70d88f05ac9f96ab618afcd
pJefersson <jefersson.hatakebs@gmail.com>
2024-10-31 12:07:25 -0300

    x

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const {select,input} = require ('@inquirer/prompts')
-const meta = {value:"Teste", checked: false}
-const metas = [meta]
+const {select, input, checkbox} = require ('@inquirer/prompts')
+let meta = {value:"Teste", checked: false}
+let metas = [meta]
+let mensangem = "Bem Vindo ao APP"
 
 const cadastrar = async () =>{
    
@@ -8,7 +9,7 @@ const cadastrar = async () =>{
         message:"Digite a meta"            
     })
     if (meta.length == 0 ){
-        console.log("A meta não pode ser vazia")
+        mensangem = "A meta não pode ser vazia"
         return
     }
    metas.push({value:meta, checked:false}) 
@@ -16,12 +17,105 @@ const cadastrar = async () =>{
 }
 
 const listarMetas = async () => {
-    console.log(metas)
+    
+    const respostas = await checkbox({
+        message:"Suas Metas: " + metas.length,
+        choices:[...metas]
+    })
+
+    metas.forEach((m)=>{
+        m.checked = false
+    })
+
+    if(respostas.length == 0 ){
+        mensangem = "Nenhuma meta selecionada"
+        
+        return
+    }
+   
+    respostas.forEach((resposta)=>{
+        const meta = metas.find((m)=>{
+            return m.value == resposta
+        })
+        meta.checked = true
+    })
+
+    mensangem = "Meta(s) marcada(s) como concluida(s)"
+    return
     
 }
 
+const metasRealizadas =  async () => {
+    const realizadas = metas.filter(()=>{
+        return meta.checked
+    })
+    
+
+    if(realizadas.length == 0){
+        mensagem = "Não existem metas realizadas! :("
+        return
+    }
+
+    await select({
+        message:"Metas Realizadas: " + realizadas.length,
+        choices: [...realizadas]
+    })
+
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta)=>{
+        return meta.checked != true
+    })
+    
+    if(abertas.length == 0) {
+        mensangem = "Não existem metas abertas :)"
+        return
+    }
+
+    await select({
+        message: "Metas Abertas: " +abertas.length,
+        choices: [...abertas]
+    })  
+    
+}
+
+const deletarMetas = async () => {
+    const metasDesmarcadas = metas.map((meta) => {
+        return {value: meta.value, checked: false}
+    })
+
+    const itensADeletar = await checkbox({
+        message:"Suas Metas ",
+        choices:[...metasDesmarcadas]
+    })
+    if(itensADeletar.length == 0){
+        mensagem = "Nenhum item para deletar!"
+        return
+    }
+    itensADeletar.forEach((item) =>{
+        metas = metas.filter((meta) => {
+            return meta.value != item
+        })
+    })
+    mensangem = "Meta(s) deletada(s) com sucesso!"
+}
+
+const mostrarMensagem = () =>{
+    console.clear();
+
+    if(mensangem != ""){
+        console.log(mensangem)
+        console.log("")
+        mensangem = ""
+    }
+
+}
+
 const start = async () =>{
+   
     while(true){
+        mostrarMensagem()
         const opcao = await select({
             message:"Menu",
             choices:[
@@ -32,6 +126,18 @@ const start = async () =>{
                 {
                     name:"Listar Metas",
                     value:"listar"
+                },
+                {
+                    name:"Metas Realizadas",
+                    value:"realizadas"
+                },
+                {
+                    name:"Metas Abertas",
+                    value:"abertas"
+                },
+                {
+                    name:"Deletar Metas",
+                    value:"deletar"
                 },
                 {
                     name:"Sair",
@@ -46,8 +152,18 @@ const start = async () =>{
                 break
             case "listar":
                 await listarMetas()
+                break
+            case "realizadas":
+                await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
+                break
+            case "deletar":
+                await deletarMetas()
+                break
             case "sair":
-                console.log("até logo")
+                console.log("Até logo")
             return
         }
     }
